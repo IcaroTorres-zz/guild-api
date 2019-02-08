9963f064fb9beab8357866c911161c28a861e4b4
icarotorres <icaro.stuart@gmail.com>
2019-02-08 19:14:07 -0300

    removed unused EqualityComparers

--- Models/Guild.cs
+++ Models/Guild.cs
@@ -10,20 +10,4 @@ namespace lumen.api.Models {
     public string MasterName { get; set; }
     public HashSet<string> Members { get; set; }
   }
-  class GuildEqualityComparer : IEqualityComparer<Guild>
-  {
-      public bool Equals(Guild g1, Guild g2)
-      {
-          if (g2 == null && g1 == null)
-            return true;
-          else if (g1 == null || g2 == null)
-            return false;
-          else if(g1.Name.Equals(g2.Name, StringComparison.OrdinalIgnoreCase))
-              return true;
-          else
-              return false;
-      }
-
-      public int GetHashCode(Guild g) => g.Name.GetHashCode();
-  }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -10,21 +10,4 @@ namespace lumen.api.Models {
     public string GuildName { get; set; }
     public bool IsGuildMaster { get; set; }
   }
-  
-  class UserEqualityComparer : IEqualityComparer<User>
-  {
-      public bool Equals(User u1, User u2)
-      {
-          if (u2 == null && u1 == null)
-            return true;
-          else if (u1 == null || u2 == null)
-            return false;
-          else if(u1.Name.Equals(u2.Name, StringComparison.OrdinalIgnoreCase))
-              return true;
-          else
-              return false;
-      }
-
-      public int GetHashCode(User u) => u.Name.GetHashCode();
-  }
 }
