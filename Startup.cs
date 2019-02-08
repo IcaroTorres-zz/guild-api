ee2376a3752172f0dcbf575803c5f15ab20e52bb
icarotorres <icaro.stuart@gmail.com>
2019-02-08 04:43:43 -0300

    register context and Inmemory provider in DI services

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using lumen.api.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,8 +27,15 @@ namespace lumen.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddEntityFrameworkInMemoryDatabase()
+                    .AddDbContext<LumenContext>((serviceProvider, options) => options.UseInMemoryDatabase("lumenInMemoryDB")
+                                                                                     .UseInternalServiceProvider(serviceProvider));
+            
+                                                                                    
+    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
