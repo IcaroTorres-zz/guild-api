d1630d2e7ddeed1451d1fef9d7c24216082da0bf
icarotorres <icaro.stuart>
2019-02-10 22:14:57 -0300

    context update and relational reconfig

--- Context/LumenContext.cs
+++ Context/LumenContext.cs
@@ -10,7 +10,17 @@ namespace lumen.api.Context {
     public DbSet<Guild> Guilds { get; set; }
     public DbSet<User> Users { get; set; }
     public LumenContext(DbContextOptions<LumenContext> options) : base(options) { }
-    public LumenContext() {}
-    protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseInMemoryDatabase("lumenInMemoryDB");
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Guild>()
+        .HasOne(g => g.Master)
+        .WithOne() // without any navigation property on User entity
+        .HasForeignKey<Guild>(g => g.MasterId);
+        // the foreignKey here is needed cause there is no navigation property on the other relation size
+
+      modelBuilder.Entity<Guild>()
+        .HasMany(g => g.Members) // navigation property 1:*
+        .WithOne(u => u.Guild); // with Guild reference navigation property on User entity
+    }
   }
 }
