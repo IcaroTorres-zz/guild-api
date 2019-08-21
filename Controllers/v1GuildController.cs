db7b85932b350c91d0f03dd45a9e77ce850aae72
icarotorres <icaro.stuart@gmail.com>
2019-08-21 08:56:51 -0300

    update all controller methods

--- Controllers/v1GuildController.cs
+++ Controllers/v1GuildController.cs
@@ -1,24 +1,25 @@
-﻿using Guild.DTOs;
-using Guild.Entities;
-using Guild.Services;
+﻿using DTOs;
+using Services;
+using Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
 
-namespace api.Controllers
+namespace Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/guilds/v1")]
     [Produces("application/json")]
     [ApiController]
-    public class v2GuildsController : ControllerBase
+    public class V1GuildsController : ControllerBase
     {
         // injected unit of work from startup.cs configure services
-        private readonly IGuildService _guildService;
-        public v2GuildsController(IGuildService service) => _guildService = service;
+        private readonly IGuildService _service;
+        public V1GuildsController(IGuildService service) => _service = service;
 
-        [HttpPost]
+        [HttpPost("")]
         public ActionResult CreateGuild([FromBody] GuildDto payload)
         {
             if (!ModelState.IsValid)
@@ -26,19 +27,21 @@ namespace api.Controllers
                                                                                           .Select(e => e.ErrorMessage))));
             try
             {
-                return Created($"{Request.Path.ToUriComponent()}/{payload.Id}", _guildService.CreateGuild(payload));
+                var guild = _service.CreateGuild(payload);
+                _service.Commit();
+                return Created($"{Request.Path.ToUriComponent()}/{guild.Id}", ResponseWithLinks(guild));
             }
             catch (InvalidOperationException e) { return RollbackAndResult409(e); }
             catch (Exception e) { return RollbackAndResult500(e); }
         }
 
         [HttpGet("{id}")]
-        public ActionResult GuildInfo(string id)
+        public ActionResult GuildInfo(Guid id)
         {
             try
             {
-                var guild = _guildService.Get<Guild, string>(id);
-                if (guild != null) return Ok(guild);
+                var guild = _service.Get<Guild>(id);
+                if (guild != null) return Ok(ResponseWithLinks(guild));
                 else return NotFound(ErrorMessageBuilder($"Guild '{id}' not found"));
             }
             catch (Exception e) { return RollbackAndResult500(e); }
@@ -47,14 +50,14 @@ namespace api.Controllers
         [HttpGet("list/{count:int=20}")]
         public ActionResult Guilds(int count)
         {
-            try { return Ok(_guildService.List(count)); }
+            try { return Ok(ResponseWithLinks(_service.List(count))); }
             catch (Exception e) { return RollbackAndResult500(e); }
         }
 
         [HttpPut("{id}")]
-        public ActionResult UpdateGuild(string id, [FromBody] GuildDto payload)
+        public ActionResult UpdateGuild(Guid id, [FromBody] GuildDto payload)
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid || payload.Id != id)
                 return BadRequest(ErrorMessageBuilder(string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors)
                                                                                           .Select(e => e.ErrorMessage))));
             if (payload.Members != null && !payload.Members.Contains(payload.MasterId))
@@ -62,61 +65,71 @@ namespace api.Controllers
 
             try
             {
-                var guild = _guildService.Get<Guild, string>(id);
-                if (guild != null)
+                var guild = _service.UpdateGuild(payload);
+                if (guild.Id == id)
                 {
-                    _guildService.Remove(guild);
-                    _guildService.Complete();
-
-                    // return Ok(_guildService.CreateGuild(payload));
-                    return Ok(_guildService.UpdateGuild(payload));
+                    _service.Commit();
+                    return Ok(ResponseWithLinks(guild));
                 }
-                else return NotFound(ErrorMessageBuilder($"Guild '{id}' not found"));
-            }
-            catch (Exception e) { return RollbackAndResult500(e); }
-        }
-
-        [HttpPatch("{id}")]
-        public ActionResult PatchGuild(string id, [FromBody] PatchDto payload)
-        {
-            var messageSuffixs = new Dictionary<PatchAction, string>()
-            {
-                { PatchAction.Add, $"to add member '{payload.userId}' in guild '{id}'" },
-                { PatchAction.Remove, $"to remove member '{payload.userId}' in guild '{id}'" },
-                { PatchAction.Transfer, $"to transfer '{id}' to member {payload.userId}" },
-            };
-
-            try
-            {
-                if (payload.Action == PatchAction.Add)
-                    _guildService.AddMember(id, payload.userId);
-                else if (payload.Action == PatchAction.Remove)
-                    _guildService.RemoveMember(id, payload.userId);
                 else
-                    _guildService.Transfer(id, payload.userId);
-
-                _guildService.Complete();
-                return Ok(true);
+                    throw new InvalidOperationException($"Conflict in keys given in Put Route and Data. [{id} != {guild.Id}]");
             }
             catch (ArgumentNullException e) { return RollbackAndResult404(e); }
             catch (InvalidOperationException e) { return RollbackAndResult409(e); }
             catch (Exception e) { return RollbackAndResult500(e); }
         }
 
-        [HttpDelete("{id}")] // DONE
-        public ActionResult DeleteGuild(string id)
+        [HttpPatch("{id}")]
+        public ActionResult PatchGuild(Guid id, [FromBody] JsonPatchDocument<Guild> patchPayload)
         {
             try
             {
-                var guild = _guildService.Get<Guild, string>(id);
+                var guild = _service.Get<Guild>(id);
+                patchPayload.ApplyTo(guild);
+
+                return Ok(ResponseWithLinks(guild));
+            }
+            catch (Exception e) { return RollbackAndResult500(e); }
+        }
+        //    var messageSuffixs = new Dictionary<PatchAction>()
+        //    {
+        //        { PatchAction.Add, $"to add member '{payload.userId}' in guild '{id}'" },
+        //        { PatchAction.Remove, $"to remove member '{payload.userId}' in guild '{id}'" },
+        //        { PatchAction.Transfer, $"to transfer '{id}' to member {payload.userId}" },
+        //    };
+
+        //    try
+        //    {
+        //        if (payload.Action == PatchAction.Add)
+        //            _service.AddMember(id, payload.userId);
+        //        else if (payload.Action == PatchAction.Remove)
+        //            _service.RemoveMember(id, payload.userId);
+        //        else
+        //            _service.Transfer(id, payload.userId);
+
+        //        _service.Complete();
+        //        return Ok(true);
+        //    }
+        //    catch (ArgumentNullException e) { return RollbackAndResult404(e); }
+        //    catch (InvalidOperationException e) { return RollbackAndResult409(e); }
+        //    catch (Exception e) { return RollbackAndResult500(e); }
+        //}
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteGuild(Guid id)
+        {
+            try
+            {
+                var guild = _service.Get<Guild>(id);
                 if (guild != null)
                 {
-                    _guildService.Remove(guild);
-                    _guildService.Complete();
-                    return NoContent();
+                    _service.Remove(guild);
+                    _service.Commit();
+                    return StatusCode(StatusCodes.Status204NoContent, ResponseWithLinks(guild));
                 }
-                else return NotFound(ErrorMessageBuilder($"Guild {id}' not Found"));
+                else throw new ArgumentNullException(nameof(Guild), $"Guild with id {id}' not Found");
             }
+            catch (ArgumentNullException e) { return RollbackAndResult404(e); }
             catch (Exception e) { return RollbackAndResult500(e); }
         }
 
@@ -127,18 +140,112 @@ namespace api.Controllers
 
         private ObjectResult RollbackAndResult404(Exception e)
         {
-            _guildService.Rollback();
+            _service.Rollback();
             return NotFound(ErrorMessageBuilder(e.Message));
         }
         private ObjectResult RollbackAndResult409(Exception e)
         {
-            _guildService.Rollback();
+            _service.Rollback();
             return Conflict(ErrorMessageBuilder(e.Message));
         }
         private ObjectResult RollbackAndResult500(Exception e)
         {
-            _guildService.Rollback();
+            _service.Rollback();
             return StatusCode(StatusCodes.Status500InternalServerError, ErrorMessageBuilder(e.Message));
+        }
+
+        private object ResponseWithLinks(IEnumerable<Entity<Guid>> entities)
+        {
+            var uri = $"{Request.Scheme}://{Request.Host}{Request.PathBase}/api/guilds/v1";
+            var Data = entities;
+            var entityType = entities.First().GetType().Name.Replace("Proxy", "");
+            return new
+            {
+                Data,
+                Links = new List<object>{
+                    new
+                    {
+                        Href = $"{uri}",
+                        Method = "Post",
+                        Intent = $"Create {entityType}",
+                        Type = entityType
+                    }
+                }
+            };
+        }
+        private object ResponseWithLinks(Entity<Guid> entity)
+        {
+            var uri = $"{Request.Scheme}://{Request.Host}{Request.PathBase}/api/guilds/v1";
+            var Data = entity;
+            var entityType = entity.GetType().Name.Replace("Proxy", "");
+            var Links = new List<object>();
+            var post = new
+            {
+                Href = $"{uri}",
+                Method = "Post",
+                Intent = $"Create {entityType}",
+                Type = entityType
+            };
+            var get = new
+            {
+                Href = $"{uri}/{entity.Id}",
+                Method = "Get",
+                Intent = $"{entityType} info",
+                Type = entityType
+            };
+            var put = new
+            {
+                Href = $"{uri}/{entity.Id}",
+                Method = "Put",
+                Intent = $"Rewrite {entityType}",
+                Type = entityType
+            };
+            var patch = new
+            {
+                Href = $"{uri}/{entity.Id}",
+                Method = "Patch",
+                Intent = $"Partially Update {entityType}",
+                Type = entityType
+            };
+            var delete = new
+            {
+                Href = $"{uri}/{entity.Id}",
+                Method = "Delete",
+                Intent = $"Delete {entityType}",
+                Type = entityType
+            };
+            var list = new
+            {
+                Href = uri + "/{count:int?}",
+                Method = "Get",
+                Intent = $"List of {entityType} items",
+                Type = entityType
+            };
+
+            switch (Request.Method.ToLower())
+            {
+                case "post":
+                    Links.AddRange(new[] { get, list, put, patch, delete });
+                    break;
+                case "get":
+                    Links.AddRange(new[] { put, patch, delete });
+                    break;
+                case "put":
+                    Links.AddRange(new[] { get, patch, delete });
+                    break;
+                case "patch":
+                    Links.AddRange(new[] { get, put, delete });
+                    break;
+                case "delete":
+                    Links.AddRange(new[] { post, list });
+                    break;
+                default:
+                    break;
+            }
+            if (Request.Method.ToLower() == "delete")
+                return new { Links, Data = new { } };
+            else
+                return new { Data, Links };
         }
     }
 }
