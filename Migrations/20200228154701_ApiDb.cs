2807e2f73ec7867ca88c85b53a5e08e10ff62a56
icarotorres <icaro.stuart@gmail.com>
2020-02-28 13:38:03 -0300

    added migration for sqlite database creation

---
+++ Migrations/20200228154701_ApiDb.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class ApiDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Memberships",
+                columns: table => new
+                {
+                    GuildId = table.Column<Guid>(nullable: false),
+                    MemberId = table.Column<Guid>(nullable: false),
+                    Entrance = table.Column<DateTime>(nullable: false),
+                    Exit = table.Column<DateTime>(nullable: true),
+                    Disabled = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memberships", x => new { x.MemberId, x.GuildId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    Disabled = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    GuildId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Guilds",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    Disabled = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    MasterId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guilds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Guilds_Users_MasterId",
+                        column: x => x.MasterId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guilds_MasterId",
+                table: "Guilds",
+                column: "MasterId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_GuildId",
+                table: "Memberships",
+                column: "GuildId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_MemberId",
+                table: "Memberships",
+                column: "MemberId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_GuildId",
+                table: "Users",
+                column: "GuildId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Memberships_Users_MemberId",
+                table: "Memberships",
+                column: "MemberId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Memberships_Guilds_GuildId",
+                table: "Memberships",
+                column: "GuildId",
+                principalTable: "Guilds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Guilds_GuildId",
+                table: "Users",
+                column: "GuildId",
+                principalTable: "Guilds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Guilds_Users_MasterId",
+                table: "Guilds");
+
+            migrationBuilder.DropTable(
+                name: "Memberships");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Guilds");
+        }
+    }
+}
