cff7f8bc5b2ced8c72b2be5679c371d8fa2ee84c
icarotorres <icaro.stuart@gmail.com>
2020-02-28 13:41:36 -0300

    include sqlite services registration

--- Startup.cs
+++ Startup.cs
@@ -7,14 +7,22 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
+using Unities;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Reflection;
 
 namespace api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration) => Configuration = configuration;
+        public Startup(IConfiguration configuration, IHostingEnvironment appHost)
+        {
+            Configuration = configuration;
+            AppHost = appHost;
+        }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment AppHost { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -23,32 +31,39 @@ namespace api
                 .AddHttpContextAccessor()
 
                 // DbContext dependency registration
-                .AddEntityFrameworkInMemoryDatabase()
-                .AddDbContext<ApiContext>(options => options.UseLazyLoadingProxies().UseInMemoryDatabase($"InMemory{nameof(ApiContext)}"))
+                .AddEntityFrameworkSqlite()
+                .AddDbContext<ApiContext>(options => options
+                    .UseLazyLoadingProxies()
+                    .UseSqlite($"Data Source={AppHost.ContentRootPath}\\{Configuration["SqliteSettings:Source"]}")
+                    .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning)))
+
+                // swagger
                 .AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "Guild.api", Version = "v1" }); })
 
                 // Custom service layer dependecy registration
                 .AddScoped<DbContext, ApiContext>()
                 .AddScoped<IBaseService, BaseService>()
                 .AddScoped<IGuildService, GuildService>()
+                .AddScoped<IUnitOfWork, UnitOfWork>()
 
                 // enabling UseLazyLoadingProxies, requires AddJsonOptions to handle navigation reference looping on json serialization
-                .AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddJsonOptions(options => 
+                .AddMvcCore()
+                .AddApiExplorer()
+                .AddJsonFormatters(options =>
                 {
-                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-                });
+                    options.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    options.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                    options.Formatting = Newtonsoft.Json.Formatting.Indented;
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IBaseService service)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseWebApiExceptionHandler(service);
             }
             else
             {
@@ -58,7 +73,8 @@ namespace api
             var swaggerOptions = new SwaggerOptions();
             Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
 
-            app.UseSwagger(option => { option.RouteTemplate = swaggerOptions.JsonRoute; })
+            app.UseWebApiExceptionHandler()
+               .UseSwagger(option => { option.RouteTemplate = swaggerOptions.JsonRoute; })
                .UseSwaggerUI(option =>
                {
                     option.SwaggerEndpoint(swaggerOptions.UiEndpoint,
