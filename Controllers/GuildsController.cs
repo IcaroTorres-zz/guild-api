4fe95337059d657d12e41ef3e16682fd6ba32b17
icarotorres <icaro.stuart@gmail.com>
2020-03-02 13:59:05 -0300

    updates to match updates on IGuildService and domain entities

--- Controllers/GuildsController.cs
+++ Controllers/GuildsController.cs
@@ -1,9 +1,9 @@
-﻿using ActionFIlters;
+﻿using Abstractions.Services;
+using ActionFilters;
 using DTOs;
-using Entities;
+using Implementations.Entities;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using Services;
 using System;
 
 namespace Controllers
@@ -11,13 +11,13 @@ namespace Controllers
     [Route("api/[controller]/v1"), ApiController]
     public class GuildsController : ControllerBase
     {
-        [HttpGet("{id}", Name = "get-guild")]
+        [HttpGet("{id}", Name = "get-guild"), CacheResponse(30)]
         public ActionResult Get(Guid id, [FromServices] IGuildService service)
         {
             return Ok(service.Get(id));
         }
 
-        [HttpGet(Name = "get-guilds")]
+        [HttpGet(Name = "get-guilds"), CacheResponse(30)]
         public ActionResult GetAll([FromServices] IGuildService service, [FromQuery(Name = "count")] int count = 20)
         {
             return Ok(service.List(count));
@@ -26,7 +26,7 @@ namespace Controllers
         [HttpPost(Name = "create-guild"), UseUnitOfWork]
         public ActionResult Create([FromBody] GuildDto payload, [FromServices] IGuildService service)
         {
-            var guild = service.CreateOrUpdate(payload);
+            var guild = service.Create(payload);
 
             return Created($"{Request.Path.ToUriComponent()}/{guild.Id}", guild);
         }
@@ -34,13 +34,13 @@ namespace Controllers
         [HttpPut("{id}", Name = "update-guild"), UseUnitOfWork]
         public ActionResult Update(Guid id, [FromBody] GuildDto payload, [FromServices] IGuildService service)
         {
-            return Ok(service.CreateOrUpdate(payload, id));
+            return Ok(service.Update(payload, id));
         }
 
         [HttpPatch("{id}", Name = "patch-guild"), UseUnitOfWork]
         public ActionResult Patch(Guid id, [FromBody] JsonPatchDocument<Guild> patchPayload, [FromServices] IGuildService service)
         {
-            return Ok(service.Update(id, patchPayload));
+            return Ok(service.Patch(id, patchPayload));
         }
 
         [HttpDelete("{id}", Name = "delete-guild"), UseUnitOfWork]
