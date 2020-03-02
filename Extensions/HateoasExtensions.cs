1868cb8d451c908c2967ccae2449c880f3694838
icarotorres <icaro.stuart@gmail.com>
2020-03-02 13:38:00 -0300

    added hateoasExtensions to extension folder

---
+++ Extensions/HateoasExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Hateoas
+{
+    public static class HateoasExtensions
+    {
+        public static IMvcCoreBuilder AddHateoasResources(this IMvcCoreBuilder builder, Action<HateoasOptions> options = null)
+        {
+            if (options != null)
+            {
+                builder.Services.Configure(options);
+            }
+            builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            builder.AddMvcOptions(o => o.OutputFormatters.Add(new JsonHateoasFormatter()));
+            return builder;
+        }
+    }
+}
