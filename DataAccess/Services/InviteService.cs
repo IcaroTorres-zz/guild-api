f15b5afdee390974527b81c06698c67ef2147671
icarotorres <icaro.stuart@gmail.com>
2020-03-15 11:55:39 -0300

    replaced private helper methods calls to get related entities by calls to BaseService methods doing the same but reusing code. refactor using expression body lambdas.

--- DataAccess/Services/InviteService.cs
+++ DataAccess/Services/InviteService.cs
@@ -6,6 +6,9 @@ using DataAccess.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using DataAccess.Entities.NullEntities;
+using DataAccess.Validations;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Services
 {
@@ -13,14 +16,12 @@ namespace DataAccess.Services
     {
         public InviteService(ApiContext context) : base(context) { }
 
-        public IInvite GetInvite(Guid id)
+        public IInvite Get(Guid id)
         {
-            var included = $"{nameof(Invite.Guild)},{nameof(Invite.Member)}" +
-                $",{nameof(Invite.Guild)}.{nameof(Invite.Guild.Members)}" +
-                $",{nameof(Invite.Member)}.{nameof(Invite.Member.Memberships)}";
-
-            return Query<Invite>(i => i.Id == id, included: included).SingleOrDefault()
-                ?? throw new KeyNotFoundException($"Target {nameof(Invite)} with id '{id}' not found.");
+            return Query<Invite>(i => i.Id == id)
+                .Include(i => i.Guild.Invites)
+                .Include(i => i.Guild.Members)
+                .Include(i => i.Member.Memberships).SingleOrDefault() ?? new NullInvite();
         }
 
         public IReadOnlyList<IInvite> List(InviteDto payload) => Query<Invite>(
@@ -31,61 +32,21 @@ namespace DataAccess.Services
         public IInvite InviteMember(InviteDto payload)
         {
             var guild = GetGuild(payload.GuildId);
-
             var member = GetMember(payload.MemberId);
+            if (guild.Invite(member) is Invite newInvite)
+            {
+                return Insert(newInvite);
+            }
 
-            return guild.Invite(member) ?? throw new ArgumentException("Can not invite member contained in inviting guild.", nameof(payload.MemberId));
+            var badInvite = new NullInvite();
+            (badInvite.ValidationResult as BadRequestValidationResult)
+                .AddValidationError(nameof(Invite),$"{nameof(Member)} is invalid or already in inviting {nameof(Guild)}.");
+            return badInvite;
         }
 
-        public IInvite Accept(Guid id)
-        {
-            var invite = GetInvite(id);
-
-            invite.BeAccepted();
-
-            return invite;
-        }
-
-        public IInvite Decline(Guid id)
-        {
-            var invite = GetInvite(id);
-
-            invite.BeDeclined();
-
-            return invite;
-        }
-
-        public IInvite Cancel(Guid id)
-        {
-            var invite = GetInvite(id);
-
-            invite.BeCanceled();
-
-            return invite;
-        }
-
-        private IMember GetMember(Guid memberId)
-        {
-            var keys = new object[] { memberId };
-            var navigations = new[] { nameof(Member.Guild) };
-            var collections = new[] { nameof(Member.Memberships) };
-
-            return GetWithKeys<Member>(keys, navigations, collections)
-                ?? throw new KeyNotFoundException($"Target {nameof(Member)} with id '{memberId}' not found.");
-        }
-
-        private IGuild GetGuild(Guid id)
-        {
-            var keys = new object[] { id };
-            var collections = new[] { nameof(Guild.Members), nameof(Guild.Invites) };
-
-            return GetWithKeys<Guild>(keys, collections: collections)
-                ?? throw new KeyNotFoundException($"Target guild with id '{id}' not found.");
-        }
-
-        public IInvite Delete(Guid id)
-        {
-            return Remove<Invite>(id);
-        }
+        public IInvite Accept(Guid id) => Get(id).BeAccepted();
+        public IInvite Decline(Guid id) => Get(id).BeDeclined();
+        public IInvite Cancel(Guid id) => Get(id).BeCanceled();
+        public IInvite Delete(Guid id) => Remove<Invite>(id);
     }
 }
