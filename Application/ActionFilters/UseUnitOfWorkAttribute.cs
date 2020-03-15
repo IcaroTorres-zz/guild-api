b1fc29960e517c178b3cbc5f8b47bea2f3968a0c
icarotorres <icaro.stuart@gmail.com>
2020-03-15 12:15:35 -0300

    refactor to inherit from ValidateResultAttribute and wrap it with unit of work transactional checks using context exception and validation errors.

--- Application/ActionFilters/UseUnitOfWorkAttribute.cs
+++ Application/ActionFilters/UseUnitOfWorkAttribute.cs
@@ -1,24 +1,34 @@
-﻿using Domain.Unities;
+﻿using DataAccess.Entities;
+using DataAccess.Validations;
+using Domain.Unities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Threading.Tasks;
 
 namespace Application.ActionFilters
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class UseUnitOfWorkAttribute : ActionFilterAttribute
+    public class UseUnitOfWorkAttribute : ValidateResultAttribute
     {
         public IUnitOfWork UnitOfWork { get; protected set; }
-
-        public override void OnActionExecuting(ActionExecutingContext context)
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             UnitOfWork = context.HttpContext.RequestServices.GetRequiredService<IUnitOfWork>().Begin();
-        }
+            
+            var executedContext = await next();
+            var okResult = executedContext.Result as OkObjectResult;
+            var createdResult = executedContext.Result as CreatedResult;
+            var entityValue =  (okResult?.Value ?? createdResult?.Value) as BaseEntity;
 
-        public override void OnActionExecuted(ActionExecutedContext context)
-        {
-            if (context.Exception == null && (context.HttpContext.Response.StatusCode < 400))
+            if (entityValue is BaseEntity && entityValue.Validate() is ErrorValidationResult errorResult)
+            {
+                executedContext.HttpContext.Response.StatusCode = (int) errorResult.Status;
+                executedContext.Result = errorResult.AsErrorActionResult();
+            }
+
+            if (executedContext.Exception == null && (executedContext.HttpContext.Response.StatusCode < 400))
             {
                 UnitOfWork.Commit();
             }
