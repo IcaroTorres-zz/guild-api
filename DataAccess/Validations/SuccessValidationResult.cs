24602ff165297253727b59d43f8a04ce7f1ecf36
icarotorres <icaro.stuart@gmail.com>
2020-03-15 12:11:41 -0300

    updated successValidationResult and removed other validations representing success. the validation as output will only be used in cases of validation errors.

--- DataAccess/Validations/SuccessValidationResult.cs
+++ DataAccess/Validations/SuccessValidationResult.cs
@@ -1,16 +1,11 @@
 using System;
 using Domain.Validations;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace DataAccess.Validations
 {
     [Serializable]
-    public abstract class SuccessValidationResult : ISuccessValidationResult, IValidationResult
+    public class SuccessValidationResult : IValidationResult
     {
         public bool IsValid => true;
-        public object Data { get; protected set; }
-        public abstract IActionResult AsActionResult(HttpRequest request);
-        public abstract IActionResult AsActionResult();
     }
 }
