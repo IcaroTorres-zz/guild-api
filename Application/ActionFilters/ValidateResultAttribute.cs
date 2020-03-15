39a5a321de89888bab2da8cd8b42ea0529f0dedf
icarotorres <icaro.stuart@gmail.com>
2020-03-15 12:13:48 -0300

    validation attribute to check validation success and change output result in case of any validation errors.

---
+++ Application/ActionFilters/ValidateResultAttribute.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Entities;
+using DataAccess.Validations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
+
+namespace Application.ActionFilters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ValidateResultAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var executedContext = await next();
+            var okResult = executedContext.Result as OkObjectResult;
+            var createdResult = executedContext.Result as CreatedResult;
+            var entityValue =  (okResult?.Value ?? createdResult?.Value) as BaseEntity;
+            if (entityValue is BaseEntity && entityValue.Validate() is ErrorValidationResult errorInsteadOkResult)
+            {
+                executedContext.Result = errorInsteadOkResult.AsErrorActionResult();
+            }
+        }
+    }
+}
