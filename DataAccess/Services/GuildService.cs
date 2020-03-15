9033b7dc98ea586d9eb1042fa147798cb8667e99
icarotorres <icaro.stuart@gmail.com>
2020-03-15 11:54:48 -0300

    added checks and validation errors on update method. replaced helper methods calls to get related entities by calls to BaseService methods doing the same but reusing code.

--- DataAccess/Services/GuildService.cs
+++ DataAccess/Services/GuildService.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using DataAccess.Validations;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Services
 {
@@ -27,12 +28,25 @@ namespace DataAccess.Services
         }
         public IGuild Update(GuildDto payload, Guid id)
         {
-            var guildToUpdate = GetGuild(id);
+            var guildToUpdate = Get(id);
             var master = GetMember(payload.MasterId);
+            if (!guildToUpdate.IsGuildMember(master))
+            {
+                var guildConflict = new ConflictValidationResult(nameof(Guild))
+                    .AddValidationError(nameof(Guild.Members), $"Cannot {nameof(Guild.Promote)} a non {nameof(Member)}.");
+
+                var memberConflict = new ConflictValidationResult(nameof(Member))
+                    .AddValidationError(nameof(Member.Guild), $"Cannot {nameof(Member.BePromoted)} due to not being member of target {nameof(Member.Guild)}.");
+
+                guildToUpdate.ValidationResult = guildConflict;
+                master.ValidationResult = memberConflict;
+            }
             guildToUpdate.Promote(master);
             guildToUpdate.ChangeName(payload.Name);
-            guildToUpdate.UpdateMembers(
-                Query<Member>(m => !m.Disabled, included: $"{nameof(Member.Memberships)},{nameof(Member.Guild)}")
+            guildToUpdate.UpdateMembers(Query<Member>(m => !m.Disabled)
+                .Include(m => m.Guild.Members)
+                .Include(m => m.Guild.Invites)
+                .Include(g => g.Memberships)
                 .Join(payload.MemberIds, m => m.Id, id => id, (member, _) => member));
 
             return guildToUpdate;
@@ -41,22 +55,7 @@ namespace DataAccess.Services
         {
             return GetAll<Guild>(included: nameof(Guild.Members), readOnly: true).Take(count).ToList();
         }
-        public IGuild GetGuild(Guid id)
-        {
-            var keys = new object[] { id };
-            var collections = new[] { nameof(Guild.Members), nameof(Guild.Invites) };
-            return GetWithKeys<Guild>(keys, collections: collections);
-        }
-        private IMember GetMember(Guid memberId)
-        {
-            var keys = new object[] { memberId };
-            var navigations = new[] { nameof(Member.Guild) };
-            var collections = new[] { nameof(Member.Memberships) };
-            return GetWithKeys<Member>(keys, navigations, collections);
-        }
-        public IGuild Delete(Guid id)
-        {
-            return Remove(GetGuild(id));
-        }
+        public IGuild Get(Guid id) => GetGuild(id);
+        public IGuild Delete(Guid id) => Remove(Get(id));
     }
 }
