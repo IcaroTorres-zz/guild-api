c5377c66fcf73887caa97865a08785b1962d319f
icarotorres <icaro.stuart@gmail.com>
2020-03-27 00:36:21 -0300

    feature(data): migration, sqlite and json files for seeding data with guid conversion to string and unique index name columns

--- DataAccess/Migrations/20200326162452_Indexes.Designer.cs
+++ DataAccess/Migrations/20200326162452_Indexes.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Api.DataAccess.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    partial class ApiContextModelSnapshot : ModelSnapshot
+    [Migration("20200326162452_Indexes")]
+    partial class Indexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
