d600ae02d3bd4c9b21a60ef459ff0d9588cb719b
icarotorres <icaro.stuart@gmail.com>
2020-03-28 23:36:10 -0300

    refactor(repositories): add 'exists' method to repositories.

--- DataAccess/Repositories/GuildRepository.cs
+++ DataAccess/Repositories/GuildRepository.cs
@@ -17,20 +17,20 @@ namespace DataAccess.Repositories
         }
         public virtual Guild Get(Guid id, bool readOnly)
         {
-            var query = Query(x => x.Id == id)
-                .Include(x => x.Members).ThenInclude(x => x.Memberships)
-                .Include(x => x.Invites).ThenInclude(x => x.Member);
+            var query = Query(x => x.Id == id);
                 
             return (readOnly ? query.AsNoTracking() : query).SingleOrDefault();
         }
         public virtual IQueryable<Guild> GetAll(bool readOnly = false) => Query(e => true, readOnly);
         public virtual IQueryable<Guild> Query(Expression<Func<Guild, bool>>? predicate = null, bool readOnly = false)
         {
-            return BaseRepository.Query(predicate, readOnly);
+            return BaseRepository.Query(predicate, readOnly)
+                .Include(x => x.Members).ThenInclude(x => x.Memberships)
+                .Include(x => x.Invites).ThenInclude(x => x.Member);
         }
         public virtual Guild Insert(DomainModel<Guild> domainModel) => BaseRepository.Insert(domainModel);
         public virtual Guild Update(DomainModel<Guild> domainModel) => BaseRepository.Update(domainModel);
         public virtual Guild Remove(DomainModel<Guild> domainModel) => BaseRepository.Remove(domainModel);
-        public bool Exists(Expression<Func<Guild, bool>> predicate) => BaseRepository.Exists(predicate);
+        public virtual bool Exists(Expression<Func<Guild, bool>> predicate) => BaseRepository.Exists(predicate);
     }
 }
--- DataAccess/Repositories/InviteRepository.cs
+++ DataAccess/Repositories/InviteRepository.cs
@@ -17,17 +17,18 @@ namespace DataAccess.Repositories
         }
         public virtual Invite Get(Guid id, bool readOnly)
         {
-            var query = Query(x => x.Id == id)
-                .Include(x => x.Member.Memberships)
-                .Include(x => x.Guild.Members).ThenInclude(m => m.Memberships);
+            var query = Query(x => x.Id == id);
 
             return (readOnly ? query.AsNoTracking() : query).SingleOrDefault();
         }
         public virtual IQueryable<Invite> Query(Expression<Func<Invite, bool>>? predicate = null, bool readOnly = false)
         {
-            return BaseRepository.Query(predicate, readOnly);
+            return BaseRepository.Query(predicate, readOnly)
+                .Include(x => x.Member.Memberships)
+                .Include(x => x.Guild.Members).ThenInclude(m => m.Memberships);
         }
         public virtual Invite Insert(DomainModel<Invite> domainModel) => BaseRepository.Insert(domainModel);
         public virtual Invite Remove(DomainModel<Invite> domainModel) => BaseRepository.Remove(domainModel);
+        public virtual bool Exists(Expression<Func<Invite, bool>> predicate) => BaseRepository.Exists(predicate);
     }
 }
