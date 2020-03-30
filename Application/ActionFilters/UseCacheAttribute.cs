ea43cbeaa9cd2d27187866d5735c0dc7d52293d8
icarotorres <icaro.stuart@gmail.com>
2020-03-30 17:18:43 -0300

    feat(action-filters): add filters implementation to cache paginated response and unit of work validations improvements.

--- Application/ActionFilters/UseCacheAttribute.cs
+++ Application/ActionFilters/UseCacheAttribute.cs
@@ -1,16 +1,9 @@
-﻿using Application.Cache;
-using Domain.Entities;
-using Domain.Models;
-using Domain.Validations;
-using Microsoft.AspNetCore.Http;
+﻿using Application.ActionFilters.Extensions;
+using Application.Cache;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.ActionFilters
@@ -30,12 +23,12 @@ namespace Application.ActionFilters
             var cacheSettings = context.HttpContext.RequestServices.GetRequiredService<RedisCacheSettings>();
             if (!cacheSettings.Enabled)
             {
-                await ExecuteNextAsync(next);
+                (await next()).EnableResultValidation();
                 return;
             }
 
             var cacheService = context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
-            var cacheKey = GenerateCacheKeyFromRequest(context.HttpContext.Request);
+            var cacheKey = context.HttpContext.Request.GenerateCacheKeyFromRequest();
             var cachedResponse = await cacheService.GetCacheResponseAsync(cacheKey);
 
             if (cachedResponse != null)
@@ -44,70 +37,12 @@ namespace Application.ActionFilters
                 return;
             }
 
-            var executedContext = await ExecuteNextAsync(next);
+            var executedContext = (await next()).EnableResultValidation();
+
             if (executedContext.Result is OkObjectResult okObjectResult)
             {
-                var valueType = okObjectResult.Value.GetType();
-                if (valueType.GetInterfaces().Contains(typeof(IEnumerable)) && valueType.GetGenericArguments().Any())
-                {
-                    okObjectResult.Value = (okObjectResult.Value as IEnumerable<object>)
-                        .Select(x => x
-                        .GetType()
-                        .GetProperty(nameof(DomainModel<Guild>.Entity))
-                        ?.GetValue(x) ?? x)
-                        .ToList();
-                }
-
                 await cacheService.CacheResponseAsync(cacheKey, okObjectResult.Value, timeToLive: TimeSpan.FromSeconds(_timeToLiveSeconds));
             }
-        }
-
-        private async Task<ActionExecutedContext> ExecuteNextAsync(ActionExecutionDelegate next)
-        {
-            var executedContext = await next();
-            var valueProperty = executedContext.Result.GetType().GetProperty("Value");
-            var value = valueProperty?.GetValue(executedContext.Result);
-            var validation = value?.GetType().GetProperty(nameof(DomainModel<Guild>.ValidationResult))?.GetValue(value);
-
-            if (validation is IApiValidationResult apiValidation)
-            {
-                if (apiValidation.IsValid)
-                {
-                    var entityValue = value?.GetType().GetProperty(nameof(DomainModel<Guild>.Entity))?.GetValue(value);
-                    executedContext.Result.GetType().GetProperty("Value")?.SetValue(executedContext.Result, entityValue);
-                }
-                else
-                {
-                    executedContext.HttpContext.Response.StatusCode = apiValidation.Status;
-                    executedContext.Result = apiValidation.AsErrorActionResult();
-                }
-            }
-            return executedContext;
-        }
-
-        private static string GenerateCacheKeyFromRequest(HttpRequest request)
-        {
-            var cacheKeyBuilder = new StringBuilder(request.Path);
-
-            if (request.Query.Any())
-            {
-                cacheKeyBuilder = request.Query
-                    .OrderBy(q => q.Key)
-                    .Aggregate(
-                        cacheKeyBuilder,
-                        (orderedQueryBuilder, currentQueryStringPair) =>
-                        {
-                            var previousFullValue = orderedQueryBuilder.Append(orderedQueryBuilder.ToString() == request.Path.ToString() ? "?" : "&").ToString();
-                            orderedQueryBuilder.Clear().Append($"{previousFullValue}{currentQueryStringPair.Key}={currentQueryStringPair.Value}");
-                            return orderedQueryBuilder;
-                        });
-            }
-
-            cacheKeyBuilder = request.Headers
-                .Where(header => !string.IsNullOrWhiteSpace(header.Value) && !header.Key.ToLowerInvariant().Contains("token"))
-                .Aggregate(cacheKeyBuilder, (headerBuilder, header) => headerBuilder.Append($"|{header.Key}={header.Value}"));
-
-            return cacheKeyBuilder.ToString();
         }
     }
 }
--- Application/ActionFilters/UseUnitOfWorkAttribute.cs
+++ Application/ActionFilters/UseUnitOfWorkAttribute.cs
@@ -1,7 +1,5 @@
-﻿using Domain.Entities;
-using Domain.Models;
+﻿using Application.ActionFilters.Extensions;
 using Domain.Unities;
-using Domain.Validations;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -15,30 +13,12 @@ namespace Application.ActionFilters
         public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             using var unitOfWork = context.HttpContext.RequestServices.GetRequiredService<IUnitOfWork>().Begin();
-
-            var executedContext = await next();
-            var valueProperty = executedContext.Result.GetType().GetProperty("Value");
-            var value = valueProperty?.GetValue(executedContext.Result);
-            var validation = value?.GetType().GetProperty(nameof(DomainModel<Guild>.ValidationResult))?.GetValue(value);
-
-            if (validation is IApiValidationResult apiValidation)
-            {
-                if (apiValidation.IsValid)
-                {
-                    var entityValue = value?.GetType().GetProperty(nameof(DomainModel<Guild>.Entity))?.GetValue(value);
-                    executedContext.Result.GetType().GetProperty("Value")?.SetValue(executedContext.Result, entityValue);
-                }
-                else
-                {
-                    executedContext.HttpContext.Response.StatusCode = apiValidation.Status;
-                    executedContext.Result = apiValidation.AsErrorActionResult();
-                }
-            }
-
+            var executedContext = (await next()).EnableResultValidation();
             if (executedContext.Exception != null || executedContext.HttpContext.Response.StatusCode >= 400)
+            {
                 unitOfWork.RollbackTransaction();
-            else
-                unitOfWork.Commit();
+            }
+            else unitOfWork.Commit();
         }
     }
 }
