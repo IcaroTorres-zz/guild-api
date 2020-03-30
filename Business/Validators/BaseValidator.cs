2e3d93426c82b6d04a51b68e33cff51d867e0b30
icarotorres <icaro.stuart@gmail.com>
2020-03-30 17:24:29 -0300

    chore(validators): replaced error code and error message in BaseValidator for disabled entities.

--- Business/Validators/BaseValidator.cs
+++ Business/Validators/BaseValidator.cs
@@ -9,9 +9,11 @@ namespace Business.Validators
     {
         protected readonly string _notFoundMessage = $"{typeof(T).Name} not found.";
         protected readonly string _conflictFoundMessage = $"{typeof(T).Name} is invalid.";
+        protected readonly string _goneMessage = $"{typeof(T).Name} was removed or disabled.";
 
         protected readonly string _notFoundCodeString = ((int)HttpStatusCode.NotFound).ToString();
         protected readonly string _conflictCodeString = ((int)HttpStatusCode.Conflict).ToString();
+        protected readonly string _goneCodeString = ((int)HttpStatusCode.Gone).ToString();
 
         public BaseValidator()
         {
@@ -23,8 +25,8 @@ namespace Business.Validators
 
             RuleFor(x => x.Disabled)
                 .NotEqual(true)
-                .WithMessage(_notFoundMessage)
-                .WithErrorCode(_notFoundCodeString);
+                .WithMessage(_goneCodeString)
+                .WithErrorCode(_goneMessage);
         }
     }
 }
