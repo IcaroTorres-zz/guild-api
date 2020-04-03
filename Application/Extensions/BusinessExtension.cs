c7e529ebbb572fab46ff3743e6c2de324722f395
icarotorres <icaro.stuart@gmail.com>
2020-04-03 12:12:26 -0300

    style: formatting whitespaces.

--- Application/Extensions/BusinessExtension.cs
+++ Application/Extensions/BusinessExtension.cs
@@ -18,32 +18,32 @@ namespace Application.Extensions
     public static IServiceCollection BootstrapPipelinesServices(this IServiceCollection services)
     {
       return services
-          // mediatR dependency injection
-          .AddMediatR(typeof(CreateGuildHandler).GetTypeInfo().Assembly)
+        // mediatR dependency injection
+        .AddMediatR(typeof(CreateGuildHandler).GetTypeInfo().Assembly)
 
-          // mediatR open types pipeline behaviors
-          .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>))
-          .AddTransient(typeof(IPipelineBehavior<,>), typeof(ResponseValidationBehavior<,>))
+        // mediatR open types pipeline behaviors
+        .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>))
+        .AddTransient(typeof(IPipelineBehavior<,>), typeof(ResponseValidationBehavior<,>))
 
-          // mediatR handlers
-          .AddTransient<IPipelineBehavior<MemberFilterCommand, ApiResponse<Pagination<Member>>>, MemberFilterHandler>()
-          .AddTransient<IPipelineBehavior<GuildFilterCommand, ApiResponse<Pagination<Guild>>>, GuildFilterHandler>()
-          .AddTransient<IPipelineBehavior<InviteFilterCommand, ApiResponse<Pagination<Invite>>>, InviteFilterHandler>()
+        // mediatR handlers
+        .AddTransient<IPipelineBehavior<MemberFilterCommand, ApiResponse<Pagination<Member>>>, MemberFilterHandler>()
+        .AddTransient<IPipelineBehavior<GuildFilterCommand, ApiResponse<Pagination<Guild>>>, GuildFilterHandler>()
+        .AddTransient<IPipelineBehavior<InviteFilterCommand, ApiResponse<Pagination<Invite>>>, InviteFilterHandler>()
 
-          .AddTransient<IPipelineBehavior<CreateMemberCommand, ApiResponse<Member>>, CreateMemberHandler>()
-          .AddTransient<IPipelineBehavior<CreateGuildCommand, ApiResponse<Guild>>, CreateGuildHandler>()
-          .AddTransient<IPipelineBehavior<InviteMemberCommand, ApiResponse<Invite>>, InviteMemberHandler>()
+        .AddTransient<IPipelineBehavior<CreateMemberCommand, ApiResponse<Member>>, CreateMemberHandler>()
+        .AddTransient<IPipelineBehavior<CreateGuildCommand, ApiResponse<Guild>>, CreateGuildHandler>()
+        .AddTransient<IPipelineBehavior<InviteMemberCommand, ApiResponse<Invite>>, InviteMemberHandler>()
 
-          .AddTransient<IPipelineBehavior<UpdateMemberCommand, ApiResponse<Member>>, UpdateMemberHandler>()
-          .AddTransient<IPipelineBehavior<UpdateGuildCommand, ApiResponse<Guild>>, UpdateGuildHandler>()
+        .AddTransient<IPipelineBehavior<UpdateMemberCommand, ApiResponse<Member>>, UpdateMemberHandler>()
+        .AddTransient<IPipelineBehavior<UpdateGuildCommand, ApiResponse<Guild>>, UpdateGuildHandler>()
 
-          .AddTransient<IPipelineBehavior<PromoteMemberCommand, ApiResponse<Member>>, PromoteMemberHandler>()
-          .AddTransient<IPipelineBehavior<DemoteMemberCommand, ApiResponse<Member>>, DemoteMemberHandler>()
-          .AddTransient<IPipelineBehavior<LeaveGuildCommand, ApiResponse<Member>>, LeaveGuildHandler>()
+        .AddTransient<IPipelineBehavior<PromoteMemberCommand, ApiResponse<Member>>, PromoteMemberHandler>()
+        .AddTransient<IPipelineBehavior<DemoteMemberCommand, ApiResponse<Member>>, DemoteMemberHandler>()
+        .AddTransient<IPipelineBehavior<LeaveGuildCommand, ApiResponse<Member>>, LeaveGuildHandler>()
 
-          .AddTransient<IPipelineBehavior<AcceptInviteCommand, ApiResponse<Invite>>, AcceptInviteHandler>()
-          .AddTransient<IPipelineBehavior<DeclineInviteCommand, ApiResponse<Invite>>, DeclineInviteHandler>()
-          .AddTransient<IPipelineBehavior<CancelInviteCommand, ApiResponse<Invite>>, CancelInviteHandler>();
+        .AddTransient<IPipelineBehavior<AcceptInviteCommand, ApiResponse<Invite>>, AcceptInviteHandler>()
+        .AddTransient<IPipelineBehavior<DeclineInviteCommand, ApiResponse<Invite>>, DeclineInviteHandler>()
+        .AddTransient<IPipelineBehavior<CancelInviteCommand, ApiResponse<Invite>>, CancelInviteHandler>();
     }
   }
 }
--- Application/Extensions/DALExtension.cs
+++ Application/Extensions/DALExtension.cs
@@ -17,22 +17,22 @@ namespace Application.Extensions
     public static IServiceCollection BootstrapDALServices(this IServiceCollection services, IWebHostEnvironment env, IConfiguration configuration)
     {
       return services
-          // DbContext dependency registration
-          .AddDbContext<ApiContext>(options =>
-          {
-            var sourcePath = Path.Combine(env.ContentRootPath, "../DAL", configuration["SqliteSettings:Source"]);
-            options.UseSqlite($"Data Source={sourcePath}");
-          })
+        // DbContext dependency registration
+        .AddDbContext<ApiContext>(options =>
+        {
+          var sourcePath = Path.Combine(env.ContentRootPath, "../DAL", configuration["SqliteSettings:Source"]);
+          options.UseSqlite($"Data Source={sourcePath}");
+        })
 
-          // Data access layer dependencies
-          .AddScoped<IRepository<Guild>, Repository<Guild>>()
-          .AddScoped<IRepository<Member>, Repository<Member>>()
-          .AddScoped<IRepository<Invite>, Repository<Invite>>()
-          .AddScoped<IRepository<Membership>, Repository<Membership>>()
-          .AddScoped<IGuildRepository, GuildRepository>()
-          .AddScoped<IMemberRepository, MemberRepository>()
-          .AddScoped<IInviteRepository, InviteRepository>()
-          .AddScoped<IUnitOfWork, UnitOfWork>();
+        // Data access layer dependencies
+        .AddScoped<IRepository<Guild>, Repository<Guild>>()
+        .AddScoped<IRepository<Member>, Repository<Member>>()
+        .AddScoped<IRepository<Invite>, Repository<Invite>>()
+        .AddScoped<IRepository<Membership>, Repository<Membership>>()
+        .AddScoped<IGuildRepository, GuildRepository>()
+        .AddScoped<IMemberRepository, MemberRepository>()
+        .AddScoped<IInviteRepository, InviteRepository>()
+        .AddScoped<IUnitOfWork, UnitOfWork>();
     }
   }
 }
