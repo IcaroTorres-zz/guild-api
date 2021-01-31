657e7726dd6ba9482397050b7be80b108f97ab78
icarotorres-ioasys <icarotorres@ioasys.com.br>
2021-01-31 12:08:36 -0300

    refactor(commands): add abstract commands to business layer sharing code for segregated command abstractions.

---
+++ Business/Commands/QueryItemCommand.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Commands;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Business.Commands
+{
+    [ExcludeFromCodeCoverage]
+    public abstract class QueryItemCommand<TIn, TOut> : MappedResultCommand<TIn, TOut>, IQueryItemCommand
+    {
+        [FromRoute(Name = "id")] public virtual Guid Id { get; set; }
+    }
+}
---
+++ Business/Commands/UpdateCommand.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Commands;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Business.Commands
+{
+    public abstract class UpdateCommand<TIn, TOut> : MappedResultCommand<TIn, TOut>, ITransactionalCommand
+    {
+        [FromRoute(Name = "id")] public Guid Id { get; set; }
+    }
+}
