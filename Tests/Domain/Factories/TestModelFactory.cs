28dff9d7847c2dce8e021ef0b371cd2ea9e066e4
icarotorres <icaro.stuart@gmail.com>
2021-03-04 20:47:54 -0300

    feat(model-factory): create factory for test versions of models with event listeners included.

---
+++ Tests/Domain/Factories/TestModelFactory.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Common;
+using Domain.Enums;
+using Domain.Models;
+using System;
+using Tests.Domain.Models.TestModels;
+
+namespace Tests.Domain.Factories
+{
+    public class TestModelFactory : IModelFactory
+    {
+        public Guild CreateGuild(string name, Member member)
+        {
+            var guild = new TestGuild { Id = Guid.NewGuid(), Name = name };
+            var invite = guild.InviteMember(member, this);
+            invite.BeAccepted(this);
+            return guild;
+        }
+
+        public Invite CreateInvite(Guild guild, Member member)
+        {
+            return new TestInvite
+            {
+                Id = Guid.NewGuid(),
+                Guild = guild,
+                GuildId = guild.Id,
+                Member = member,
+                MemberId = member.Id,
+                Status = InviteStatuses.Pending,
+            };
+        }
+
+        public Member CreateMember(string name)
+        {
+            return new TestMember { Id = Guid.NewGuid(), Name = name };
+        }
+
+        public Membership CreateMembership(Guild guild, Member member)
+        {
+            return new TestMembership
+            {
+                Id = Guid.NewGuid(),
+                Guild = guild,
+                GuildId = guild.Id,
+                Member = member,
+                MemberId = member.Id,
+                CreatedDate = DateTime.UtcNow,
+            };
+        }
+    }
+}
---
+++ Tests/Domain/Factories/TestModelFactoryTests.cs
@@ -0,0 +1,100 @@
+﻿using Domain.Enums;
+using Domain.Nulls;
+using FluentAssertions;
+using Tests.Domain.Models.Fakes;
+using Tests.Domain.Models.TestModels;
+using Tests.Helpers;
+using Xunit;
+
+namespace Tests.Domain.Factories
+{
+    [Trait("Domain", "Model-Factory")]
+    public class TestModelFactoryTests
+    {
+        private readonly TestModelFactory _sut;
+
+        public TestModelFactoryTests()
+        {
+            _sut = TestModelFactoryHelper.Factory;
+        }
+
+        [Fact]
+        public void CreateGuild_WithNameAndMember_Should_Create_With_AllProperties()
+        {
+            // arrange
+            const string expectedName = "name";
+            var member = MemberFake.WithoutGuild().Generate();
+
+            // act
+            var model = _sut.CreateGuild(expectedName, member);
+
+            // assert
+            model.Should().NotBeNull().And.BeOfType<TestGuild>();
+            model.Id.Should().NotBeEmpty();
+            model.Name.Should().NotBeEmpty().And.Be(expectedName);
+            model.Members.Should().Contain(member);
+            model.Invites.Should().Contain(x => x.MemberId == member.Id && x.GuildId == model.Id);
+        }
+
+        [Fact]
+        public void CreateInvite_WithGuildAndMember_Should_Create_Pending_With_AllProperties()
+        {
+            // arrange
+            var guild = GuildFake.Valid().Generate();
+            var member = MemberFake.WithoutGuild().Generate();
+            const InviteStatuses expectedStatus = InviteStatuses.Pending;
+
+            // act
+            var model = _sut.CreateInvite(guild, member);
+
+            // assert
+            model.Should().NotBeNull().And.BeOfType<TestInvite>();
+            model.Id.Should().NotBeEmpty();
+            model.Guild.Should().NotBeNull().And.Be(guild);
+            model.GuildId.Should().NotBeEmpty().And.Be(guild.Id);
+            model.Member.Should().NotBeNull().And.Be(member);
+            model.MemberId.Should().NotBeEmpty().And.Be(member.Id);
+            model.Status.Should().Be(expectedStatus);
+        }
+
+        [Fact]
+        public void CreateMembership_WithGuildAndMember_Should_CreateWith_AllProperties_Except_ModifiedDate()
+        {
+            // arrange
+            var guild = GuildFake.Valid().Generate();
+            var member = MemberFake.WithoutGuild().Generate();
+
+            // act
+            var model = _sut.CreateMembership(guild, member);
+
+            // assert
+            model.Should().NotBeNull().And.BeOfType<TestMembership>();
+            model.Id.Should().NotBeEmpty();
+            model.ModifiedDate.Should().BeNull();
+            model.Guild.Should().Be(guild);
+            model.GuildId.Should().NotBeEmpty().And.Be(guild.Id);
+            model.Member.Should().Be(member);
+            model.MemberId.Should().NotBeEmpty().And.Be(member.Id);
+        }
+
+
+        [Fact]
+        public void Constructor_WithName_Should_CreateWith_Name_Id()
+        {
+            // arrange
+            const string expectedName = "testname";
+
+            // act
+            var model = _sut.CreateMember(expectedName);
+
+            // assert
+            model.Should().NotBeNull().And.BeOfType<TestMember>();
+            model.Id.Should().NotBeEmpty();
+            model.Name.Should().NotBeEmpty().And.Be(expectedName);
+            model.IsGuildLeader.Should().BeFalse();
+            model.GuildId.Should().BeNull();
+            model.Guild.Should().BeOfType<NullGuild>();
+            model.Memberships.Should().BeEmpty();
+        }
+    }
+}
