c75886affab9ad1e986ea8e7ba2fae02c02df5c3
icarotorres <icaro.stuart@gmail.com>
2021-03-04 20:51:21 -0300

    feat(model-factory): add a static test model factory instance accessible through all tests.

---
+++ Tests/Helpers/TestModelFactoryHelper.cs
@@ -0,0 +1,9 @@
+﻿using Tests.Domain.Factories;
+
+namespace Tests.Helpers
+{
+    public static class TestModelFactoryHelper
+    {
+        public static readonly TestModelFactory Factory = new TestModelFactory();
+    }
+}
