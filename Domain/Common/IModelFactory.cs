b299bddc3be98ac8c532a0b687f84ff3592c9c48
icarotorres <icaro.stuart@gmail.com>
2021-03-04 20:41:19 -0300

    feat(model-factory): moved constructor logic from models to a model factory.

---
+++ Domain/Common/IModelFactory.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Domain.Common
+{
+    public interface IModelFactory
+    {
+        Guild CreateGuild(string name, Member member);
+        Member CreateMember(string name);
+        Invite CreateInvite(Guild guild, Member member);
+        Membership CreateMembership(Guild guild, Member member);
+    }
+}
---
+++ Domain/Factories/ModelFactory.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Common;
+using Domain.Enums;
+using Domain.Models;
+using System;
+
+namespace Domain.Factories
+{
+    public class ModelFactory : IModelFactory
+    {
+        public Guild CreateGuild(string name, Member member)
+        {
+            var guild = new Guild { Id = Guid.NewGuid(), Name = name };
+            var invite = guild.InviteMember(member, this);
+            invite.BeAccepted(this);
+            return guild;
+        }
+
+        public Invite CreateInvite(Guild guild, Member member)
+        {
+            return new Invite
+            {
+                Id = Guid.NewGuid(),
+                Guild = guild,
+                GuildId = guild.Id,
+                Member = member,
+                MemberId = member.Id,
+                Status = InviteStatuses.Pending,
+            };
+        }
+
+        public Member CreateMember(string name)
+        {
+            return new Member { Id = Guid.NewGuid(), Name = name };
+        }
+
+        public Membership CreateMembership(Guild guild, Member member)
+        {
+            return new Membership
+            {
+                Id = Guid.NewGuid(),
+                Guild = guild,
+                GuildId = guild.Id,
+                Member = member,
+                MemberId = member.Id,
+                CreatedDate = DateTime.UtcNow,
+            };
+        }
+    }
+}
