e1bf4420880f9255b595d1a7c1008bc79dd8b62d
icarotorres <icaro.stuart@gmail.com>
2021-03-04 20:42:38 -0300

    feat(model-factory): add model factory service registration to dependency injection.

--- Infrastructure/DependencyInjection/ApplicationExtension.cs
+++ Infrastructure/DependencyInjection/ApplicationExtension.cs
@@ -1,16 +1,19 @@
 using Application.Common.PipelineBehaviors;
 using Application.Guilds.Commands.CreateGuild;
+using Domain.Common;
+using Domain.Factories;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Infrastructure.DependencyInjection
 {
-    public static class BusinessExtension
+    public static class ApplicationExtension
     {
         public static IServiceCollection BootstrapPipelinesServices(this IServiceCollection services)
         {
             return services
+                .AddScoped<IModelFactory, ModelFactory>()
                 // mediatR dependency injection
                 .AddMediatR(typeof(CreateGuildHandler).GetTypeInfo().Assembly)
 
